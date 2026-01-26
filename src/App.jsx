218ec43be9436faa1ed612d3068cbfdbaba75faa
Kaushik Reddy <kaushik4432@gmail.com>
2026-01-26 11:24:59 +0530

    fix: add ActionFeedbackProvider to App.jsx to prevent crash

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import { ActionFeedbackProvider } from '@/components/feedback/ActionFeedbackContext';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -73,12 +74,14 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <NavigationTracker />
-          <AuthenticatedApp />
-        </Router>
-        <Toaster />
-        <VisualEditAgent />
+        <ActionFeedbackProvider>
+          <Router>
+            <NavigationTracker />
+            <AuthenticatedApp />
+          </Router>
+          <Toaster />
+          <VisualEditAgent />
+        </ActionFeedbackProvider>
       </QueryClientProvider>
     </AuthProvider>
   )
